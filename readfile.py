eda10c240100ded0c818f5bb6d47d602eda6dbfb
ChristosCM <christos.christou-mavropoulos@durham.ac.uk>
2018-11-20 14:48:55 +0000

    Working fine/// Only change would be that the matrix is only half populated and could be added

---
+++ readfile.py
@@ -0,0 +1,66 @@
+import numpy as np
+import re 
+#fname = input("Please give the name of the file: ")
+f = open("AItestcase.txt", "r")
+
+lines = []
+#Reading lines while ingoring blank lines and white spaces in between and also not inputing \n in the list
+for line in f.readlines():
+    line = line.strip()
+    if line:
+        lines.append(line)
+ 
+numbers = ""
+
+for line in lines:
+    for char in line:
+        if char.isdigit() or char ==",":
+            numbers += char
+
+sizestr = ""
+fnumber = False
+for char in numbers:
+    #this ensures that if there exists a comma before the size, the for loop will still find the correct size
+    if char.isdigit()==True:
+        fnumber = True
+    if char != "," and fnumber == True:
+        sizestr += char
+    else:
+        if fnumber==True:
+            break
+
+size = int(sizestr)
+print("Size: " , size)
+
+numbersList = numbers.split(",")
+for item in numbersList:
+    try:
+        item = int(item)
+    except:
+        pass
+
+#getting rid of initial coma and of the size
+del numbersList[0]
+del numbersList[0]
+
+#create the matrix
+matrix = np.zeros([size-1,size])
+
+
+#populate the matrix 
+#know when to stop populating the matrix
+counter = size
+#where the numbers should start from
+start = 1
+city = 0
+generalcounter = 0
+while counter>0:
+    for x in range (start,size):
+        matrix[city,x] = numbersList[generalcounter]
+        generalcounter += 1
+    counter -= 1
+    city += 1
+    start += 1
+
+print(matrix)
+    
